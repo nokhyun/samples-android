3fb7da721ed1036ad1ad4287387a16880ce934d6
kwangjun <kwangjun.cho@ikoob.com>
2023-07-06 16:45:01 +0900

    컨트롤러 분리

--- playground1/app/src/main/java/com/nokhyun/playground1/MainActivity.kt
+++ playground1/app/src/main/java/com/nokhyun/playground1/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.nokhyun.playground1
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
-import androidx.databinding.ViewDataBinding
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.nokhyun.playground1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -44,66 +34,4 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        })
     }
-}
-
-interface ScreenSizeController {
-    fun topLevelScreenSizeInit(isTablet: Boolean, binding: ViewDataBinding, navController: NavController, onResult: () -> Unit)
-}
-
-class ScreenSizeControllerImpl : ScreenSizeController {
-
-    private val navigationViewHelper2 = NavigationViewHelper2()
-
-    override fun topLevelScreenSizeInit(isTablet: Boolean, binding: ViewDataBinding, navController: NavController, onResult: () -> Unit) {
-        val children = (binding.root as ViewGroup).children
-
-        when {
-            children.any { it is NavigationView } -> {
-                val navigationView = navigationViewHelper2.asNavigationView<NavigationView>(children)
-                initTable(navigationView!!, navController)
-            }
-
-            children.any { it is BottomNavigationView } -> {
-                val bottomNavigationView = navigationViewHelper2.asNavigationView<BottomNavigationView>(children)
-                initMobile(bottomNavigationView!!, navController)
-            }
-
-            else -> throw NavigationViewException("Navigation view inconsistency")
-        }
-
-        onResult()
-    }
-
-    private inline fun <reified R> NavigationViewHelper2.asNavigationView(children: Sequence<View>): R? = getNavigationView(children)
-
-    private fun initTable(navigationView: NavigationView, navController: NavController) {
-        navigationView.setupWithNavController(navController)
-        logger { "Tablet" }
-    }
-
-    private fun initMobile(bottomNavigationView: BottomNavigationView, navController: NavController) {
-        bottomNavigationView.setupWithNavController(navController)
-        logger { "Mobile" }
-    }
-
-    private inner class NavigationViewHelper2 {
-        inline fun <reified R> getNavigationView(view: Sequence<View>): R? {
-            return view.find { it is R } as R
-        }
-    }
-
-
-    private class NavigationViewException(message: String) : Exception(message)
-}
-
-internal fun String.show(view: View) {
-    Snackbar.make(view, this, 5000).show()
-}
-
-private fun String.logger() {
-    Log.e("Log.e", this)
-}
-
-internal fun logger(msg: () -> Any) {
-    msg().toString().logger()
 }
