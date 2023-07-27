58562bb6de8935082c455748620d8ca72b3e0814
kwangjun <kwangjun.cho@ikoob.com>
2023-07-27 14:49:48 +0900

    arrow 이미지 추가

--- playground1/feature/third/src/main/java/com/nokhyun/third/composable/PassengerItem.kt
+++ playground1/feature/third/src/main/java/com/nokhyun/third/composable/PassengerItem.kt
@@ -1,27 +1,32 @@
 package com.nokhyun.third.composable
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.nokhyun.third.AirlineUiState
+import com.nokhyun.third.R
 
 @Composable
 fun PassengerItem(
@@ -30,49 +35,91 @@ fun PassengerItem(
     onClick: () -> Unit
 ) {
     airline?.also {
-        Row(
+        Column(
             modifier = modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .clickable(
-                    indication = CustomIndication,
-                    interactionSource = remember { MutableInteractionSource() },
-                    onClick = onClick
-                )
+                .fillMaxSize()
         ) {
-            AsyncImage(
+            Row(
                 modifier = Modifier
-                    .border(BorderStroke(1.dp, Color.Black))
-                    .align(CenterVertically),
-                model = airline.logo,
-                contentDescription = null,
-                contentScale = ContentScale.FillHeight
-            )
+                    .fillMaxSize()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.CenterVertically),
+                    model = airline.logo,
+                    error = painterResource(id = R.drawable.ic_baseline_personal_video_24),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight,
+                )
 
-            Spacer(
-                modifier = modifier
-                    .padding(start = 8.dp)
-            )
+                Box(
+                    modifier = Modifier
+                        .weight(0.8f)
+                        .background(Color.DarkGray)
+                ) {
+                    Spacer(
+                        modifier = modifier
+                            .padding(start = 8.dp)
+                    )
 
-            Column {
-                Text(text = "name: ${airline.name}")
-                Text(text = "country: ${airline.country}")
-                Text(text = "slogan: ${airline.slogan}")
-                Text(text = "head_quaters: ${airline.headQuaters}")
-                Text(text = "website: ${airline.website}")
-                if(airline.expended.value) {
-                    Spacer(modifier = Modifier.padding(8.dp))
-                    Divider(
-                        modifier = Modifier
-                            .padding(start = 4.dp, end = 4.dp),
-                        color = Color.Gray,
-                        thickness = 1.dp)
-                    Spacer(modifier = Modifier.padding(4.dp))
-                    Text(text = "Expended!!!")
+                    Column {
+                        Text(text = "name: ${airline.name}")
+                        Text(text = "country: ${airline.country}")
+                        Text(text = "slogan: ${airline.slogan}")
+                        Text(text = "head_quaters: ${airline.headQuaters}")
+                        Text(text = "website: ${airline.website}")
+                    }
                 }
+
+                Image(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.1f)
+                        .clickable(
+                            indication = CustomIndication,
+                            interactionSource = remember { MutableInteractionSource() },
+                            onClick = onClick
+                        ),
+                    contentScale = ContentScale.Inside,
+                    painter = painterResource(id = R.drawable.ic_arrow),
+                    contentDescription = null,
+                    alignment = Alignment.CenterEnd
+                )
+            }
+
+            if (airline.expended.value) {
+                Spacer(modifier = Modifier.padding(8.dp))
+                Divider(
+                    modifier = Modifier
+                        .padding(start = 4.dp, end = 4.dp),
+                    color = Color.Gray,
+                    thickness = 1.dp
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(text = "Expended!!!")
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun PassengerItemPreview() {
+    PassengerItem(
+        modifier = Modifier,
+        airline = AirlineUiState.Airline(
+            name = "name",
+            country = "country",
+            logo = "",
+            slogan = "",
+            headQuaters = "",
+            website = ""
+        ),
+        onClick = {}
+    )
 }
 
 data class Airline(
