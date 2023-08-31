79193e76e35fb6db34902c8a6309b60adbf7fcc2
nokhyun90 <nokhyun90@gmail.com>
2023-08-31 23:06:30 +0900

    AnnotatedClickableText

--- playground1/feature/third/src/main/java/com/nokhyun/third/composable/ThirdContent.kt
+++ playground1/feature/third/src/main/java/com/nokhyun/third/composable/ThirdContent.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
@@ -19,12 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.nokhyun.third.ThirdViewModel
 import com.nokhyun.third.asAirline
+import com.nokhyun.third.composable.text.AnnotatedClickableText
 
 @Composable
 internal fun ThirdContent(
@@ -33,10 +34,16 @@ internal fun ThirdContent(
 ) {
     val result = thirdViewModel.result.collectAsLazyPagingItems()
     val isVisible = result.loadState.refresh == LoadState.Loading
-    Switch(checked = true, onCheckedChange = {
-    }, thumbContent = {
-        Icon(imageVector = Icons.Default.Check, contentDescription = null)
-    })
+
+    Row {
+        Switch(checked = true, onCheckedChange = {
+        }, thumbContent = {
+            Icon(imageVector = Icons.Default.Check, contentDescription = null)
+        })
+
+        AnnotatedClickableText()
+    }
+
 
     val myAppState = rememberMyAppState()
     myAppState.myApp.state
---
+++ playground1/feature/third/src/main/java/com/nokhyun/third/composable/text/Texts.kt
@@ -0,0 +1,39 @@
+package com.nokhyun.third.composable.text
+
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import com.nokhyun.third.composable.logger
+
+@Composable
+fun AnnotatedClickableText(){
+    val annotatedText = buildAnnotatedString {
+        append("Click ")
+
+        pushStringAnnotation(
+            tag = "URL", annotation = "https://developer.android.com"
+        )
+        withStyle(
+            style = SpanStyle(
+                color = Color.Blue, fontWeight = FontWeight.Bold
+            )
+        ){
+            append("here")
+        }
+
+        pop()
+    }
+
+    ClickableText(text = annotatedText, onClick = { offset ->
+        logger { "offset: $offset" }
+        annotatedText.getStringAnnotations(
+            tag = "URL", start = offset, end = offset
+        ).firstOrNull()?.let { annotation ->
+            logger { "Clicked URL: ${annotation.item}"}
+        }
+    })
+}
