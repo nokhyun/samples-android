fc3dfe1449fab497117b1e40781abeeba7f857e3
kwangjun <kwangjun.cho@ikoob.com>
2023-09-19 16:03:51 +0900

    hilt repoProvider -> binds

--- playground1/data/fakePaging/src/main/java/com/nokhyun/fakepaging/FakePagingRemoteDataSourceImpl.kt
+++ playground1/data/fakePaging/src/main/java/com/nokhyun/fakepaging/FakePagingRemoteDataSourceImpl.kt
@@ -3,13 +3,14 @@ package com.nokhyun.fakepaging
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 internal interface FakePagingRemoteDataSource {
     fun fetchPassenger(passengerData: PassengerData): Flow<PassengerResponse>
 }
 
-internal class FakePagingRemoteDataSourceImpl(
-    private val fakePagingService: FakePagingService
+internal class FakePagingRemoteDataSourceImpl @Inject constructor(
+    @FakePaging private val fakePagingService: FakePagingService
 ) : FakePagingRemoteDataSource {
     override fun fetchPassenger(passengerData: PassengerData): Flow<PassengerResponse> = flow {
         Log.e("fetchPassenger", "fetchPassenger")
