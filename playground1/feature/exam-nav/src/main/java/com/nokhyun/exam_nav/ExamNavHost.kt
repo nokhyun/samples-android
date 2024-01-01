257aa822bbd0bc8ff188dcd21894a3f888307701
nokhyun90 <nokhyun90@gmail.com>
2024-01-01 22:05:36 +0900

    - bottomsheet dragHandle

--- playground1/feature/exam-nav/src/main/java/com/nokhyun/exam_nav/ExamNavHost.kt
+++ playground1/feature/exam-nav/src/main/java/com/nokhyun/exam_nav/ExamNavHost.kt
@@ -2,10 +2,29 @@ package com.nokhyun.exam_nav
 
 import android.app.PendingIntent
 import android.content.Intent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.TaskStackBuilder
 import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -17,9 +36,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun ExamNavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController,
 ) {
     val id = "exampleId"
     val context = LocalContext.current
@@ -35,17 +55,60 @@ fun ExamNavHost(
         getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
     }
 
-    NavHost(
-        navController = navHostController,
-        startDestination = IntroScreens.First.route
-    ) {
-        composable(IntroScreens.First.route) {
-            Button(onClick = { navHostController.navigate("Home") }) {
-                Text(text = "Hello World!")
-            }
+    var isShow by remember { mutableStateOf(false) }
+
+    Column {
+        if (isShow) {
+            ModalBottomSheet(
+                sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false),
+//                tonalElevation = 24.dp,
+//                scrimColor = Color.Red,
+                onDismissRequest = {
+                    isShow = false
+                },
+                content = {
+                    val numbers = mutableListOf<String>().apply {
+                        repeat(100) {
+                            add(it.toString())
+                        }
+                    }
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        items(numbers.size) {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(12.dp),
+                                text = numbers[it],
+                                fontSize = 28.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                },
+                dragHandle = null
+            )
         }
 
-        mainGraph(navHostController)
+        Button(onClick = { isShow = true }) {
+            Text(text = "Show!")
+        }
+
+        NavHost(
+            navController = navHostController,
+            startDestination = IntroScreens.First.route
+        ) {
+            composable(IntroScreens.First.route) {
+                Button(onClick = { navHostController.navigate("Home") }) {
+                    Text(text = "Hello World!")
+                }
+            }
+
+            mainGraph(navHostController)
+        }
     }
 }
 
