b93217f250bf5718b01feed7f5339123707a44c9
nokhyun90 <nokhyun90@gmail.com>
2024-02-07 23:09:07 +0900

    canvasExam line

---
+++ playground1/feature/uiExam/src/main/java/com/nokhyun/uiexam/CanvasExam/CanvasExam.kt
@@ -0,0 +1,28 @@
+package com.nokhyun.uiexam.CanvasExam
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CanvasExamScreen() {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+    ) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        drawLine(
+            start = Offset(x = canvasWidth, y = 0f),
+            end = Offset(x = 0f, y = canvasHeight),
+            color = Color.Black
+        )
+    }
+}
--- playground1/feature/uiExam/src/main/java/com/nokhyun/uiexam/UiExamFragment.kt
+++ playground1/feature/uiExam/src/main/java/com/nokhyun/uiexam/UiExamFragment.kt
@@ -6,10 +6,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
@@ -23,12 +26,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.nokhyun.exam_nav.ExamNavActivity
+import com.nokhyun.uiexam.CanvasExam.CanvasExamScreen
 import com.nokhyun.uiexam.RememberCoroutineScope.RememberCoroutineScopeScreen
 import com.nokhyun.uiexam.disposableEffectExam.DisposableEffectScreen
 import com.nokhyun.uiexam.draggableExam.DraggableScreen
@@ -129,6 +135,7 @@ fun ExamUI(
             LaunchedEffectScreen()
             DraggableScreen()
             SwipeableScreen()
+            CanvasExamScreen()
         }
     }
 }
