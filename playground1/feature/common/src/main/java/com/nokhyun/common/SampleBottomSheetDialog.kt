7979151a7462395757b13f11c96f63604006cfb4
nokhyun90 <nokhyun90@gmail.com>
2024-05-15 22:13:07 +0900

    Added:clipboard copy example

--- playground1/feature/common/src/main/java/com/nokhyun/common/SampleBottomSheetDialog.kt
+++ playground1/feature/common/src/main/java/com/nokhyun/common/SampleBottomSheetDialog.kt
@@ -1,9 +1,13 @@
 package com.nokhyun.common
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.nokhyun.common.databinding.DialogSampleBottomSheetBinding
@@ -29,7 +33,19 @@ class SampleBottomSheetDialog : BottomSheetDialogFragment() {
                     message = "Hello World!",
                     drawable = ContextCompat.getDrawable(requireContext(), android.R.drawable.ic_lock_lock)!!
                 ).show()
+
+                copyText("Copy!")
             }
+        }
+    }
+
+    private fun copyText(text: String){
+        val cm = requireContext().getSystemService(ClipboardManager::class.java)
+
+        cm.setPrimaryClip(ClipData.newPlainText("", text))
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU){
+            Toast.makeText(requireContext(), "Copied to Clipboard", Toast.LENGTH_SHORT).show()
         }
     }
 }
